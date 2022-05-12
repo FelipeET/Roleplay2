ba2a4bfb712c84fbae339256769051fa61120fed
[FelipeET] <felipe.etchevers@correo.ucu.edu.uy>
2022-05-12 00:33:12 -0300

    Tests Items Act

---
+++ test/StaffTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System;
+using RoleplayGame;
+
+namespace RolePlayTests
+    {
+        public class StaffTest
+        {
+            [Test]
+            public void DefensaValida()
+            {
+                IDefenseItem stafo = new Staff();
+                const int expected = 100;
+                Assert.AreEqual(expected, stafo.DefenseValue);
+            }
+
+            public void AtaqueValido()
+            {
+                IAttackItem stafo = new Staff();
+                const int expected = 100;
+                Assert.AreEqual(expected, stafo.AttackValue);
+            }
+        }
+    }
